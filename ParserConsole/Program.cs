8c24e50a2927448335d7459c90cd2c19e2c15b23
Dan M. <dan@morris2morris.com>
2024-08-09 08:57:24 -0400

    testing sonarlint.xml (#2)
    Co-authored-by: Dan Morris <>

--- ParserConsole/Program.cs
+++ ParserConsole/Program.cs
@@ -23,6 +23,7 @@ namespace ParserConsole
     private static void HandleParseError(IEnumerable<Error> obj)
     {
       Console.WriteLine("Exception parsing the options provided.");
+      Console.WriteLine("Exception parsing the options provided. Exception parsing the options provided.Exception parsing the options provided.Exception parsing the options provided.");
     }
 
     private static void VerifyArguments(Options options)
